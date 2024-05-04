bca64ec14badb57831120aca0c0b9e66cb19ce6d
Ricardo Garcia <ricardonunesgarcia@outlook.com>
2024-05-04 07:37:59 -0300

    feat: cria lista de itens

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import logo from './assets/logoPlanejeAproveite.png';
 
+const initialItems = [
+  { id: 1, description: 'Passports', quantity: 2, packed: false },
+  { id: 2, description: 'Socks', quantity: 12, packed: false },
+  { id: 3, description: 'Charger', quantity: 1, packed: true },
+];
+
 export default function App() {
   return (
     <div className="app">
@@ -16,15 +22,47 @@ function Logo() {
 }
 
 function Form() {
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>O que você precisa ou quer fazer em sua viagem?</h3>
-    </div>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="Item..." />
+      <button>+</button>
+    </form>
   );
 }
 
 function PackingList() {
-  return <div className="list"></div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: 'line-through' } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 }
 
 function Stats() {
